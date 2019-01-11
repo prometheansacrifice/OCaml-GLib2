1f59a782fc0eedb69e62cb6eb7bb03b7a91d62cf
Manas <manas@likewyss.com>
2019-01-11 14:19:28 +0530

    wip

--- config/discover.ml
+++ config/discover.ml
@@ -15,11 +15,8 @@ let () =
         }
       in
       let default_ffi : C.Pkg_config.package_conf =
-        { libs   = ["-lffi"] ;
-          cflags = ["-O2"; "-Wall"; "-Wextra"; "-Wno-unused-parameter";
-                    "-I/usr/lib/libffi-3.2.1/include";
-                    "-I/usr/include/x86_64-linux-gnu"; (* default ubuntu *)
-                    "-I/usr/include"]                  (* default ubuntu *)
+        { libs   = [] ;
+          cflags = []                  (* default ubuntu *)
         }
       in
       let conf =
@@ -32,11 +29,9 @@ let () =
           let glib = Option.value query ~default in
           let  module P = C.Pkg_config in
           { libs =
-              (libffi.P.libs @ glib.P.libs) @
-              ["-L/usr/lib/x86_64-linux-gnu";"-L/usr/lib"];
+              (libffi.P.libs @ glib.P.libs);
             cflags =
-              (libffi.P.cflags @ glib.P.cflags) @
-              ["-I/usr/include"; "-I/usr/include/x86_64-linux-gnu"]
+              (libffi.P.cflags @ glib.P.cflags)
           }
       in
       let list_of_string_to_sexp = sexp_of_list sexp_of_string in
