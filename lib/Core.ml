d0060b04aca11f7ec1059f50e19b3fad04582552
Manas <manas@likewyss.com>
2019-01-23 15:00:43 +0530

    Adds more ds

--- lib/Core.ml
+++ lib/Core.ml
@@ -40,20 +40,20 @@ let filename_from_uri uri =
 let c_strlen =
   foreign "strlen" (ptr char @-> returning int)
 
-let string_to_char_ptr str =
-  let open Memcpy in
-  let len = String.length str in
-  let dst = allocate_n char (len + 1) in (* Ctypes.allocate* allocate zero filled mem.*)
-  let src = Bytes.of_string str in
-  let () = unsafe_memcpy ocaml_bytes pointer src dst len in
-  dst
-
-let char_ptr_to_string _ptr =
-  let open Memcpy in
-  let len = c_strlen _ptr in
-  let buf = Bytes.make len '\000' in
-  let () = unsafe_memcpy pointer ocaml_bytes _ptr buf len in
-  Bytes.to_string buf
+(* let string_to_char_ptr str =
+ *   let open Memcpy in
+ *   let len = String.length str in
+ *   let dst = allocate_n char (len + 1) in (\* Ctypes.allocate* allocate zero filled mem.*\)
+ *   let src = Bytes.of_string str in
+ *   let () = unsafe_memcpy ocaml_bytes pointer src dst len in
+ *   dst
+ * 
+ * let char_ptr_to_string _ptr =
+ *   let open Memcpy in
+ *   let len = c_strlen _ptr in
+ *   let buf = Bytes.make len '\000' in
+ *   let () = unsafe_memcpy pointer ocaml_bytes _ptr buf len in
+ *   Bytes.to_string buf *)
 
 (** Hash table utilities
  *  see https://developer.gnome.org/glib/stable/glib-Hash-Tables.html#g-direct-equal
